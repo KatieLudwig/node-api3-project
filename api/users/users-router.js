5abb7c32b8e127a7d4d79dec2cdd8d21979683cd
Katie Ludwig <katierludwig@gmail.com>
2024-06-30 19:37:50 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -12,10 +12,10 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   User.get()
-  .then(users => {
+    .then(users => {
       res.json(users);
     })
-   .catch(next);
+    .catch(next);
 });
 
 router.get('/:id', validateUserId, (req, res) => {
@@ -23,35 +23,34 @@ router.get('/:id', validateUserId, (req, res) => {
 });
 
 router.post('/', validateUser, (req, res, next) => {
-User.insert({ name: req.name })
-.then(newUser => {
-  res.status(201).json(newUser);
-})
-.catch(next)
+  User.insert({ name: req.name })
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   User.update(req.params.id, { name: req.name })
-  .then(() => {
-    return User.getById(req.params.id);
-  })
-  .then (user => {
-    res.json(user);
-  })
-   .catch(next);
+    .then(() => {
+      return User.getById(req.params.id);
+    })
+    .then(user => {
+      res.json(user);
+    })
+    .catch(next);
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user);
-
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id);
+    res.json(req.user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
-  console.log(req.user);
 
 });
 
